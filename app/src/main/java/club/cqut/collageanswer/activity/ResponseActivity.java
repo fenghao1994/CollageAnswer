dafa41597f4d3f8fa6f5a927976f812a7dc59373
Leero <Howe@Howe-PC>
2015-07-02 11:19:49 +0800

    修改布局，添加回答页面

---
+++ app/src/main/java/club/cqut/collageanswer/activity/ResponseActivity.java
@@ -0,0 +1,23 @@
+package club.cqut.collageanswer.activity;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+import club.cqut.collageanswer.R;
+
+
+/**
+ * 回答问题页面
+ * Created by Leero on 2015/7/2.
+ */
+@EActivity(R.layout.activity_response)
+public class ResponseActivity extends Activity {
+
+    @AfterViews
+    public void init(){
+
+    }
+
+}
